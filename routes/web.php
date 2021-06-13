b677bcf286405f89dbfacddebb1325a4881e4380
chandieka <candiekatasbihyantra2@gmail.com>
2021-06-13 08:29:13 +0700

    Update Web Route

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\GalleryController as ApiGalleryController;
+use App\Http\Controllers\GalleryController;
 use App\Http\Controllers\HelpController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SearchController;
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\UploadController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -19,25 +22,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return redirect('/home');
-});
-
 Auth::routes();
 
-// home route
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
-// help route
+// Route::get('/test', [TestController::class, 'index'])->name('test');
+
 Route::get('/help', [HelpController::class, 'index'])->name('help');
 
-// Search route
-Route::get('/search', [SearchController::class, 'index'])->name('search');
+Route::get('/search', [SearchController::class, 'index'])->name('search.index');
+
+Route::get('/galleries/{gallery}', [GalleryController::class, 'show'])->name('galleries.show');
+
+Route::get('/test', [ApiGalleryController::class, 'index']);
 
 // Route that need Authentication to be used
 Route::middleware(['auth'])->group(function () {
-    Route::get('users/{user}', [UserController::class, 'show'])->name('users.show');
+    Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
+    Route::get('/uploads', [UploadController::class, 'index'])->name('uploads.index');
+    Route::get('/gallerries/create', [GalleryController::class, 'create'])->name('galleries.create');
 });
 
-// Test Route for View, Backend logic and etc
-Route::get('/test', [TestController::class, 'index'])->name('test');
+
