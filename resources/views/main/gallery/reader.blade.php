8262e25b68817b05f871ed16b83592bd41617868
chandieka <candiekatasbihyantra2@gmail.com>
2021-06-23 09:22:44 +0700

    Move reader into the "main.gallery" subview

--- resources/views/main/gallery/reader.blade.php
+++ resources/views/main/gallery/reader.blade.php
@@ -6,16 +6,16 @@
 <div class="reader">
     @include('layouts.subviews.gallery.reader.pagination')
     <div class="reader-img">
-        <img src="{{ asset('img/default/NotFound-720p.png') }}" alt="page-1" id="reader-page" class="point">
+        <img src="{{ asset($pages[$paginator['currentPage'] - 1]['filename']) }}" alt="page-1" id="reader-page" class="point">
     </div>
     @include('layouts.subviews.gallery.reader.pagination')
 </div>
 @endsection
 @section('scripts')
 <script>
-    var gallery = @json($gallery);
-    var pages = @json($pages);
-    var paginator = @json($paginator);
+    const gallery = @json($gallery);
+    const pages = @json($pages);
+    const paginator = @json($paginator);
 </script>
 <script src="{{ asset('js/reader/eventHandler.js') }}"></script>
 @endsection
