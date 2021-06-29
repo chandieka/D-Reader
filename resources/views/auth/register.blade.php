c76bf1646f57e98083988a8198354b5a344f1fd8
chandieka <candiekatasbihyantra2@gmail.com>
2021-06-29 12:33:14 +0700

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,37 +5,23 @@
 @section('content')
 <div class="content-auth">
     @include('auth.header')
-    <ul class="error-list">
-        <li class="error">
-            <span class="error-message">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </span>
-            <span class="error-close"><i class="fas fa-times"></i></span>
-        </li>
-        <li class="error">
-            <span class="error-message">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </span>
-            <span class="error-close"><i class="fas fa-times"></i></span>
-        </li>
-    </ul>
     <form method="POST" action="{{ route('register') }}">
         @csrf
         <div class="form-group">
             <label for="name" class="label">{{ __('Name') }}</label>
-            <input id="name" type="text" class="form-control large" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+            <input id="name" type="text" class="form-control large" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="Username...">
         </div>
         <div class="form-group">
             <label for="email" class="label">{{ __('E-Mail Address') }}</label>
-            <input id="email" type="email" class="form-control large" name="email" value="{{ old('email') }}" required autocomplete="email">
+            <input id="email" type="email" class="form-control large" name="email" value="{{ old('email') }}" required autocomplete="email"  placeholder="Pattern: /.*@.*">
         </div>
         <div class="form-group">
             <label for="password" class="label">{{ __('Password') }}</label>
-            <input id="password" type="password" class="form-control large" name="password" required autocomplete="new-password">
+            <input id="password" type="password" class="form-control large" name="password" required autocomplete="new-password" placeholder="Password...">
         </div>
         <div class="form-group">
             <label for="password-confirm" class="label">{{ __('Confirm Password') }}</label>
-            <input id="password-confirm" type="password" name="password_confirmation" class="form-control large" required autocomplete="new-password">
+            <input id="password-confirm" type="password" name="password_confirmation" class="form-control large" required autocomplete="new-password" placeholder="Re-password...">
         </div>
         <div class="form-group">
             <span>Already have an account?</span>
@@ -48,4 +34,8 @@
         </div>
     </form>
 </div>
+@endsection
+
+@section('scripts')
+    <script src="{{ asset('js/error/eventHandler.js') }}"></script>
 @endsection
