2414c98e6ca8be1c118ca2dc2576db9067e722f9
chandieka <candiekatasbihyantra2@gmail.com>
2021-06-29 12:32:06 +0700

    Remove HelpController and migrate the functionalities to HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -43,4 +43,9 @@ class HomeController extends Controller
         ];
         return view('main.home', $data);
     }
+
+    public function help()
+    {
+        return view('main.help');
+    }
 }
