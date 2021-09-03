f03d73826e0f340392a8769dcc46a45f16f776e2
chandieka <candiekatasbihyantra2@gmail.com>
2021-09-03 23:12:30 +0700

    Add Intervention/Image library
    the Library is intended to be use for Thumbnail related work and other in the future

--- config/app.php
+++ config/app.php
@@ -165,7 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Intervention\Image\ImageServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -227,7 +227,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        // Extras or 3rd party packages
+        'Image' => Intervention\Image\Facades\Image::class,
     ],
 
 ];
