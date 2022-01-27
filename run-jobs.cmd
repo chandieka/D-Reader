15fa92256d377870f532c278f0deaea6479a07cd
chandieka <candiekatasbihyantra2@gmail.com>
2022-01-27 16:47:04 +0700

    Add scripts to launch job worker

--- run-jobs.cmd
+++ run-jobs.cmd
@@ -1,1 +1,6 @@
-php D:\1mportant\WebProject\D-Reader\artisan queue:work database
+php artisan queue:work database
+
+@REM uncomment and copy the line below if you want to run multiple worker
+@REM start cmd /k "echo Listening for Jobs  && php D:\1mportant\WebProject\D-Reader\artisan queue:work database"
+@REM start cmd /k "echo Listening for Jobs  && php D:\1mportant\WebProject\D-Reader\artisan queue:work database"
+
