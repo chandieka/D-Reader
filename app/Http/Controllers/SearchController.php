5e662621d11b3e747d0d15e52c1b10dc1870b635
chandieka <candiekatasbihyantra2@gmail.com>
2022-01-28 00:04:40 +0700

    Add feature for galleries search from the nav bar

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,13 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Gallery;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
     public function index(Request $request)
     {
-        dd($request->all());
-        return view('main.search');
+        $request->validate([
+            'query' => "required|string"
+        ]);
+        $request->flash();
+
+        $query = $request->input('query');
+
+        $data = [];
+
+        $data['query'] = $query;
+
+        $galleries = Gallery::whereRaw("MATCH(title_original, title) AGAINST(? IN BOOLEAN MODE)", [$query])->paginate(24);
+
+        $data['galleries'] = $galleries;
+        $data['paginator'] = [
+            'currentPage' => $galleries->currentPage(),
+            'totalPages' => $galleries->currentPage(),
+            'uri' => route('search.index') . "/?query=$query&page=", // URI template for page navigation
+            'lastPage' => $galleries->lastPage(),
+        ];
+
+        return view('main.search', $data);
     }
 }
--- resources/views/main/search.blade.php
+++ resources/views/main/search.blade.php
@@ -1,8 +1,22 @@
 @extends('layouts.app')
 
-@section('title', 'Search: ')
+@section('title', "Search: $query")
 
 @section('content')
-    @include('layouts.subviews.search-navigation')
-    @include('layouts.subviews.dashboard')
+<div class="container center mt-sm mb-sm">
+    <div class="container-title">
+        <h1 class="inline-block p-sm pl-med r-font-big">
+            Search:
+            <span style="color: rgb(204, 51, 51)">
+                {{ \App\Customs\Utils::stringShortener($query, 100) }}
+            </span>
+        </h1>
+        <h1 class="details inline-block p-sm pr-med pl-med r-font-big">
+            {{ $galleries->count() }}
+        </h1>
+    </div>
+</div>
+@include('layouts.subviews.pagination')
+@include('layouts.subviews.dashboard')
+@include('layouts.subviews.pagination')
 @endsection
