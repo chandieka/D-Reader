568af28a7de661319704ba019bd09626e0c72435
chandieka tasbihyantra <candiekatasbihyantra2@gmail.com>
2022-02-03 05:11:01 +0400

    fix bug and change small styling on the manager page

--- public/js/manager/eventHandler.js
+++ public/js/manager/eventHandler.js
@@ -1,12 +1,14 @@
 window.addEventListener("load", (e) => {
     let form = document.querySelector('#form-delete');
-    let formButton = document.querySelector('.form-delete-button');
+    let formButtons = document.querySelectorAll('.form-delete-button');
 
-    formButton.addEventListener('click', (j) => {
-        j.preventDefault()
+    formButtons.forEach(element => {
+        element.addEventListener('click', (j) => {
+            j.preventDefault()
 
-        if (confirm("WARNING!!! This action will permanently delete the item")){
-            form.submit();
-        }
-    })
+            if (confirm("WARNING!!! This action will permanently delete the item")){
+                form.submit();
+            }
+        })
+    });
 });
