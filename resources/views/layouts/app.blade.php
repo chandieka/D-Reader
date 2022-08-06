b0324b3c04802d71b320502cbb3dd1270b2a8319
chandieka <candiekatasbihyantra2@gmail.com>
2022-08-06 11:19:59 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,9 @@
     <!-- Fonts -->
     {{-- <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet"> --}}
+    {{-- Icon Styles --}}
+    {{-- Credit to: https://fontawesome.com/ --}}
+    <link href="{{ asset('fontawesome/css/all.css') }}" rel="stylesheet">
     <!-- Styles -->
     <link href="{{ asset('css/main.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
