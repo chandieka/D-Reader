0e59f772da318393678c955bae685c070671874d
chandieka <candiekatasbihyantra2@gmail.com>
2022-08-10 22:04:13 +0200

    Add a distinction between hidden and public galleries

--- resources/views/layouts/subviews/dashboard.blade.php
+++ resources/views/layouts/subviews/dashboard.blade.php
@@ -5,12 +5,9 @@
     @isset($galleries)
     <div class="card-collections">
         @foreach ($galleries as $gallery )
-        <a href="{{ route('galleries.show', $gallery->id) }}" class="card standard card-black">
+        <a href="{{ route('galleries.show', $gallery->id) }}" class="card standard card-black @if($gallery->isHidden) card-hidden @endif">
             <div class="card-container">
                 <div class="card-thumb">
-                    {{-- <img src="{{ ($gallery->thumbnail == null) ? asset('img/default/NotFound-720p.png')) }}" alt="Not Found"> --}}
-                    {{-- WARNING: it will broke if gallery dont have pages --}}
-                    {{-- <img src="{{ asset('/assets/galleries/' . $gallery->dir_path . '/' . $gallery->pages[0]->filename) }}" alt="Not Found"> --}}
                     @if (isset($gallery->thumbnail))
                         <img src="{{ asset('/assets/thumbnails/' . $gallery->dir_path . '/' . $gallery->thumbnail) }}" alt="thumbnail">
                     @else
@@ -23,6 +20,11 @@
                     </p>
                 </div>
             </div>
+            @if($gallery->isHidden)
+            <div class="card-hidden-icon">
+                <i class="far fa-eye-slash fa-6x"></i>
+            </div>
+            @endif
         </a>
         @endforeach
     </div>
