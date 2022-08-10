e2f8b7a0b6e7b7e20e93fe1a25ff56399f4f29be
chandieka <candiekatasbihyantra2@gmail.com>
2022-08-11 00:00:25 +0200

    Revert "Delete unused policies"
    This reverts commit 28826d99c342cb650fd5bd97850f8f6dcb5d24e5.

---
+++ app/Policies/ArchivePolicy.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Archive;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ArchivePolicy
+{
+    use HandlesAuthorization;
+
+
+    /**
+     * Run a check before all policy for...
+     *
+     * @param \app\Models\User $user
+     * @return mixed
+     */
+    public function before(User $user)
+    {
+        // something
+    }
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Archive  $archive
+     * @return mixed
+     */
+    public function view(User $user, Archive $archive)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Archive  $archive
+     * @return mixed
+     */
+    public function update(User $user, Archive $archive)
+    {
+        return $user->id === $archive->user_id;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Archive  $archive
+     * @return mixed
+     */
+    public function delete(User $user, Archive $archive)
+    {
+        return $user->id === $archive->user_id;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Archive  $archive
+     * @return mixed
+     */
+    public function restore(User $user, Archive $archive)
+    {
+        return $user->id === $archive->user_id;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Archive  $archive
+     * @return mixed
+     */
+    public function forceDelete(User $user, Archive $archive)
+    {
+        return $user->id === $archive->user_id;
+    }
+
+    /**
+     * Determine whether the user can process the archive into a gallery.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Archive  $archive
+     * @return mixed
+     */
+    public function process(User $user, Archive $archive)
+    {
+        return $user->id === $archive->user_id;
+    }
+}
